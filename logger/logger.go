5fe287383d8c2e4ea95ca2d0006cf8ce038f0368
pajohnson <paul.johnson@williamhill.co.uk>
2019-04-14 16:29:27 +0100

    added mongo express to docker-compose

--- logger/logger.go
+++ logger/logger.go
@@ -5,24 +5,24 @@ import (
 	"os"
 	"runtime"
 	"strings"
-
+    // "github.com/Franco-Poveda/logrus-splunk-hook"
 	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
 	// "github.com/weekface/mgorus"
 	lib "github.com/paulsjohnson91/challenge-accepted/shared"
 )
 
 //Logger hook
-func Logger() *logrus.Logger {
+func Logger() *log.Logger {
 	if err := godotenv.Load(); err != nil {
-		logrus.Info("Error loading .env file! Try get a path...")
+		log.Info("Error loading .env file! Try get a path...")
 		if err2 := godotenv.Load(lib.GetPath() + "/.env"); err2 != nil {
-			logrus.Info("Fail...")
+			log.Info("Fail...")
 			//os.Exit(1)
 		}
 	}
-	logger := logrus.New()
+	logger := log.New()
 	// hooker, err := mgorus.NewHookerWithAuth("127.0.0.1:27017", "gorest", "logs", "adminuser", "admpass1")
 	// if err == nil {
 	// 	logger.Hooks.Add(hooker)
@@ -30,7 +30,7 @@ func Logger() *logrus.Logger {
 	// 	fmt.Print(err)
 	// }
 	logger.SetReportCaller(true)
-	logger.SetFormatter(&logrus.TextFormatter{
+	logger.SetFormatter(&log.TextFormatter{
 		TimestampFormat: "15:04:05",
 		FullTimestamp:   true,
 		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
@@ -40,6 +40,12 @@ func Logger() *logrus.Logger {
 			return "", fmt.Sprintf("%s:%d", filename, f.Line)
 		},
 	})
+	// logger.SetFormatter(&log.JSONFormatter{})
+
+
+	// splunkClient := splunk.NewClient(nil, cfg.SplunkUrl, cfg.SplunkToken, cfg.SplunkSource, cfg.SplunkSourcetype, cfg.SplunkIndex)
+	// splunkHook := splunk.NewHook(splunkClient, []log.Level{InfoLevel})
+	// log.AddHook(splunkHook)
 
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
@@ -49,3 +55,20 @@ func Logger() *logrus.Logger {
 	// logger.SetLevel(log.InfoLevel)
 	return logger
 }
+
+// func (h *Hook) Fire(entry *logrus.Entry) error {
+// 	event := map[string]interface{}{
+// 		"message": entry.Message,
+// 		"time": entry.Time.String(),
+// 		"level": entry.Level.String(),
+
+// 	}
+// 	for k, v := range entry.Data {
+// 		event[k] = v
+// 	}
+
+// 	err := h.Client.Log(
+// 		event,
+// 	)
+// 	return err
+// }
