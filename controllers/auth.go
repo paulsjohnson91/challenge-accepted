caac4c5c1493fabf8321ef85dead3a4b35e0cd83
pajohnson <paul.johnson@williamhill.co.uk>
2019-05-13 12:22:59 +0100

    fix

--- controllers/auth.go
+++ controllers/auth.go
@@ -30,7 +30,7 @@ func Home() http.HandlerFunc {
 }
 
 //Auth get a valid token and expire
-func Auth1(s *db.Dispatch) http.HandlerFunc {
+func Auth(s *db.Dispatch) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		setupCORS(&w, r)
 		if (*r).Method == "OPTIONS"{
@@ -53,7 +53,6 @@ func Auth1(s *db.Dispatch) http.HandlerFunc {
 			fmt.Fprintf(w, `{"message": %q}`, err)
 			return
 		}
-        log.Info("here")
 		//write json
 		w.WriteHeader(http.StatusCreated)
 		fmt.Fprintf(w, `{"token":%q, "expire":%s, "firstname":%s, "lastname":%s, "admin":%s, "userid":%s}`, t.Token, t.Expire, t.FirstName, t.LastName, t.Admin, t.UserID)
